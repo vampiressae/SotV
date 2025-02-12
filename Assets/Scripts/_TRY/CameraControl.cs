c63b7ead2d7d791dc28fe00c6a98c0f6d387c94e
vampiressae@yahoo.com <Holycrap73>
2025-02-12 08:01:44 +0200

    testing around, game over popup

---
+++ Assets/Scripts/_TRY/CameraControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Transform _t;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _t = transform;
+        _camera = _t.GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+
+
+        _camera.transform.LookAt(_t.position);
+    }
+}
